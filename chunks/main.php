8ae2c84024e8441cce516e71e1318bf053695398
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-07-27 04:01:51 +0600

    front-end basic design done!
    header, form, and list-header created and designed...

--- chunks/main.php
+++ chunks/main.php
@@ -1,3 +1,26 @@
 <main>
-    
+    <div class="row">
+        <div class="col-8">
+            <div class="container">
+                <div class="list-div">
+                    <h2>Your To-Do List</h2>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+                <div class="form-div">
+                    <h2>ADD NEW TASK</h2>
+                    <div class="container">
+                        <form class="form-container" action="" method="post">
+                            <input type="text" class="form-control" name="task_name" id="task_name" placeholder="TASK NAME" required>
+                            <input type="time" class="form-control" name="task_time" id="task_time"  required>
+                            <input type="date" class="form-control" name="task_date" id="task_date"  required>
+                            <textarea class="form-control" name="description" id="description" cols="30" rows="5" placeholder="Describe the Task here . . ."></textarea>
+                            <input type="submit" class="form-control btn btn-primary" name="submit">
+                        </form>
+                    </div>
+                </div>
+        </div>
+    </div>
 </main>
